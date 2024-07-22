7dec2757485f7f70177a19fdcb8195ff55bca876
Matthew Phillips <matthew@skypack.dev>
2024-07-22 12:20:43 -0400

    Allow delay to be passed via query params

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,4 +2,4 @@ const API_ENDPOINT = 'https://meek-gumption-c04779.netlify.app/vacations';
 
 type APIPart = 'reviews' | 'recommendations' | 'rooms';
 
-export const fetchAPI = (part: APIPart) => fetch(API_ENDPOINT + '?part=' + part);
+export const fetchAPI = (part: APIPart, delay: number) => fetch(API_ENDPOINT + '?part=' + part + '&delay=' + delay);
