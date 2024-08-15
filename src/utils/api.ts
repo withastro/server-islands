a470662c987fe04e3f9d44097347c2aa557d17e2
Matthew Phillips <matthew@skypack.dev>
2024-08-15 13:59:08 -0400

    Revert "Merge pull request #1 from withastro/api-impl"
    This reverts commit 9ae2bf5e76efc93d868e406385898378b70489f7, reversing
    changes made to 5868a9bfa12442dfc9af6c9088bdbf04c09f4191.
    
    This simplifies the implementation so that everything is in a single
    repo, and a single hosted project. It also aligns with the explanation
    of how it works on the site more correctly.

--- src/utils/api.ts
+++
@@ -1,5 +0,0 @@
-const API_ENDPOINT = 'https://meek-gumption-c04779.netlify.app/vacations';
-
-type APIPart = 'reviews' | 'recommendations' | 'rooms';
-
-export const fetchAPI = (part: APIPart, delay: number) => fetch(API_ENDPOINT + '?part=' + part + '&delay=' + delay);
